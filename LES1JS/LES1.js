b4d059482df5e2324e36440e88b6be0d372bab80
En4IKxD <en4ikxd@gmail.com>
2024-02-27 16:12:13 +0200

    Homework

---
+++ LES1JS/LES1.js
@@ -0,0 +1,31 @@
+// let v1 = 'hello';
+// let v2 = 'owu';
+// let v3 = 'com';
+// let v4 = 'ua';
+// let v5 = 1;
+// let v6 = 10;
+// let v7 = -999;
+// let v8 = 123;
+// let v9 = 3.14;
+// let v10 = 2.7;
+// let v11 = 16;
+// let v12 = 5>4;
+// let v13 = 5>6;
+// console.log(v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13);
+
+// let firstName = 'Михайло';
+// let middleName= 'Оселедець';
+// let lastName = 'Андрійович';
+// let person = firstName+middleName+lastName
+// console.log(person);
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a, typeof b, typeof c);
+
+// let znak1 = prompt('Введіть своє ім*я');
+// let znak2 = prompt('Введіть По-Батькові');
+// let znak3 = prompt('Введіть рік народження');
+// console.log(znak1, znak2, znak3)
