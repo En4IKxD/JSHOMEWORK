93b0274bd96772bc857fe4fe0ece1ec18b8149c5
En4IKxD <en4ikxd@gmail.com>
2024-02-29 21:17:52 +0200

    Homework

---
+++ LES3JS/LES3.js
@@ -0,0 +1,39 @@
+// let x = prompt('Введіть число');
+// if (x != 0 ){
+//     console.log('Вірно')
+// } else {
+//     console.log('Невірно')
+// }
+
+// let time = prompt('Введіть час');
+// if (time >= 0 && time <= 15) {
+//     console.log('Перша Четвертина')
+// } else if (time>15 && time<=30) {
+//     console.log('Друга Четвертина')
+// }  else if (time>30 && time<=45) {
+//     console.log('Третя Четвертина')
+// } else if (time>45 && time<=59) {
+//     console.log('Четверта Четвертина')
+// }
+
+// let day = prompt('Введіть день');
+// if (day >= 1 && day <= 10) {
+//     console.log('Перша Декада')
+//     } else if (day >= 11 && day <= 20) {
+//     console.log('Друга Декада')
+//     } else if (day >= 21 && day <= 31) {
+//     console.log('Третя Декада')
+//     }
+
+// let a = prompt('Введіть перше число');
+// let b = prompt('Введіть друге число');
+// if (a>b) {
+//     console.log(a)
+// } else if (a<b) {
+//     console.log(b)
+// } else if (a=b) {
+//     console.log('Значення рівні');
+// }
+
+let x = prompt('Введіть х');
+    console.log(x || 'default'); // <<<<<<<<<<<<<<< Не зовсім зрозумів суть завдання, або занадто складно і я не розумію, або занадто просто і я сам собі перемудрив, але здається виконав.
